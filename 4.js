91380818fe55a3fbc4b00d29ee41366188de277e
Patrick Smith <patrick@burntcaramel.com>
2017-11-03 10:46:22 +1100

    Add extra word count example

--- 4.js
+++ 4.js
@@ -3,5 +3,16 @@ function wordCount(string) {
   return words.length
 }
 
+function wordCountTable(string) {
+  const words = string.split(' ')
+  let wordCounts = {}
+  words.forEach((word) => {
+    wordCounts[word] = (wordCounts[word] || 0) + 1
+  })
+  return wordCounts
+}
+
 const sentence = 'the brown fox jumps over the lazy dog'
 console.log(`There are ${wordCount(sentence)} words in: ${sentence}`)
+
+console.log(wordCountTable(sentence))
--- 4.rb
+++ 4.rb
@@ -3,5 +3,16 @@ def word_count(string)
   words.count
 end
 
+def word_count_table(string)
+  words = string.split(' ')
+  word_counts = Hash.new(0)
+  words.each do |word|
+    word_counts[word] += 1
+  end
+  word_counts
+end
+
 sentence = 'the brown fox jumps over the lazy dog'
 puts "There are #{word_count(sentence)} words in: #{sentence}"
+
+puts word_count_table(sentence)
